59345cd0c017ec1719896faa2252a03270ec471f
tarekhossen71 <97015458+tarekhossen71@users.noreply.github.com>
2022-02-05 16:59:30 +0600

    Add files via upload

---
+++ assets/js/owljs.js
@@ -0,0 +1,35 @@
+$('.portfolio-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: [
+        'Prev',
+        'Next'
+    ],
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.testimonial-1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
